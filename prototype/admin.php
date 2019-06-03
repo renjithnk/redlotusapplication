e658d7b853cc67466840f547b87de307e65c1868
Renjith NKS <renjithnks@gmail.com>
2019-06-03 12:25:55 +0530

    Admin Login

---
+++ prototype/admin.php
@@ -0,0 +1,31 @@
+<?php include('includes/header-common.php'); ?>
+
+
+<div class="container-fluid main-index-wrapper">
+<div class="row">
+<div class="col-lg-12">
+<div class="main-index-data">
+<img class="logo img-fluid" src="../../assets/images/logo.png" alt="" width="200" height="121">
+<form>
+<div class="form-group">
+<label for="exampleInputEmail1">Email address</label>
+<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+</div>
+<div class="form-group">
+<label for="exampleInputPassword1">Password</label>
+<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+</div>
+<button type="submit" class="btn btn-primary red-button">Submit</button>
+</form>
+
+        
+</div>
+</div>
+</div> 
+</div> 
+
+
+
+
+
